d51b889fa60ea2ea90b8f36dc14e5ab7a410fe2c
JKolya <nicholi@gmail.com>
2012-11-29 19:48:12 -0600

    cleanup

--- project/controllers/projects.py
+++ project/controllers/projects.py
@@ -26,6 +26,11 @@ def page_not_found(e):
     return (render_template('errors/404.html'), 404)
 
 
+@app.errorhandler(403)
+def page_not_found(e):
+    return (render_template('errors/403.html'), 403)
+
+
 @app.errorhandler(500)
 def internal_server_error(e):
     return (render_template('errors/500.html'), 500)
@@ -44,8 +49,6 @@ def mongotest():
     body = "\n".join(last_few)
     return render_template('main/mongo.html', body=body)
 
-@app.route('/con')
-def myconn():
-    return
+
 
 
